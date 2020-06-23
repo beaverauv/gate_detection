36e9cdfca9ee94e41242c7af40c37c1ff13587cf
fascettij <44498011+fascettij@users.noreply.github.com>
2020-06-23 16:58:51 -0400

    Update find_poles.py

--- find_poles.py
+++ find_poles.py
@@ -4,6 +4,7 @@ import numpy as np
 from filter_img import *
 from slope import *
 import math
+import time
 
 
 def find_and_narrow_down_lines(img, dis_factor, debug):
@@ -19,9 +20,13 @@ def find_and_narrow_down_lines(img, dis_factor, debug):
 	# run canny from gray (no blur)
 	img_canny = cv2.Canny(img_gray, 50,150, apertureSize=3)
 	
-
 	# find lines in  x1/y1/x2/y2
 	lines = cv2.HoughLinesP(img_canny, rho = 1, theta = 1*np.pi/180, threshold = 20, lines = 1, minLineLength = 100, maxLineGap = 15)
+	"""
+	for x in range(0,len(lines)):
+		for x1,y1,x2,y2 in lines[x]:
+			cv2.line(orig_img, (x1,y1),(x2,y2),(177,237,157),2) # some lime green color
+	"""
 
 	# if no lines found
 	if lines is None:
@@ -52,6 +57,7 @@ def find_and_narrow_down_lines(img, dis_factor, debug):
 	# check for if there are more x1/y1/x2/y2 data points than others, not sure how this would happen but I'm sure it would cause problems if it did
 	if len(x1_list) != len(y1_list) != len(x2_list) != len(y2_list):
 		print("An error has occured regarding x y coordnates of the detected lines.")
+		print("Review x1/y1/x2/y2 list creations.")
 		print("Exiting...")
 		sys.exit()
 
@@ -76,13 +82,14 @@ def find_and_narrow_down_lines(img, dis_factor, debug):
 		print("final x1_list without repeats: " + str(x1_list))
 		#cv2.imshow("orig_img", orig_img)
 
-	"""
-	temp_img = orig_img
-	for coord in range(len(x1_list)):
-		cv2.line(temp_img, (x1_list[coord],y1_list[coord]),(x2_list[coord],y2_list[coord]),(0,0,157),1) # some lime green color
 	if debug == True:
-		cv2.imshow("initial line detections", temp_img)
-	"""
+		""" shows narrowed down lines """
+		temp_img = orig_img
+		for coord in range(len(x1_list)):
+			cv2.line(temp_img, (x1_list[coord],y1_list[coord]),(x2_list[coord],y2_list[coord]),(0,0,157),1) # some lime green color
+		if debug == True:
+			cv2.imshow("initial line detections", temp_img)
+	
 	
 	return(len(x1_list), orig_len, img, x1_list, y1_list, x2_list, y2_list)
 
@@ -207,12 +214,13 @@ def find_horizontal(x1_list, y1_list, x2_list, y2_list, refined_slopes, first_po
 def touch_up(first_pole, second_pole, horizontal, orig_img, confidence, debug):
 	ext_percent = 0
 	center = None
+	synth_horizontal = None
 	# if one pole is found (under first_pole) make pole_missing = to 1, if both missing = 0 or if both found = 2
 	if second_pole == []:
 		pole_missing = 1
 		if first_pole == []:
 			pole_missing = pole_missing - 1
-			return(orig_img, ext_percent, first_pole, second_pole, center)
+			return(orig_img, ext_percent, first_pole, second_pole, center, synth_horizontal)
 	else:
 		pole_missing = 2
 		
@@ -271,9 +279,8 @@ def touch_up(first_pole, second_pole, horizontal, orig_img, confidence, debug):
 
 	elif len(horizontal) == 4:
 		
-		first_pole_horz_intersect = line_intersection(first_pole, horizontal)
-		print(first_pole_horz_intersect)
-		second_pole_horz_intersect = line_intersection(second_pole, horizontal)
+		first_pole_horz_intersect = line_intersection(first_pole, horizontal, debug)
+		second_pole_horz_intersect = line_intersection(second_pole, horizontal, debug)
 		
 		ext1 = []
 		ext2 = []
@@ -323,23 +330,40 @@ def touch_up(first_pole, second_pole, horizontal, orig_img, confidence, debug):
 		horz_len = round(line_length(horizontal))
 		ext_percent = round(horz_len/total_ext_len)
 
-	# add legend
-	cv2.putText(orig_img, 'poles', (5,25), cv2.FONT_HERSHEY_SIMPLEX,  0.75, (0,0,255), 1, cv2.LINE_AA)
-	if len(horizontal) == 4:
-		cv2.putText(orig_img, 'horizontal har (detected)', (5,50), cv2.FONT_HERSHEY_SIMPLEX,  0.75, (255,0,0), 1, cv2.LINE_AA)
-		cv2.putText(orig_img, 'horizontal bar fill', (5,75), cv2.FONT_HERSHEY_SIMPLEX,  0.75, (177,237,157), 1, cv2.LINE_AA)
-	elif synth_horizontal != []:
-		cv2.putText(orig_img, 'predicted horizontal bar', (5,50), cv2.FONT_HERSHEY_SIMPLEX,  0.75, (177,237,157), 1, cv2.LINE_AA)
-
+	
 	# add circles at corners
-	if horizontal != []:
+	if len(horizontal) == 4:
 		cv2.circle(orig_img, (ext1[0], ext1[1]), 5, (255,255,255), 4)
 		cv2.circle(orig_img, (ext2[0], ext2[1]), 5, (255,255,255), 4)
 	elif len(synth_horizontal) == 4:
 		cv2.circle(orig_img, (synth_horizontal[0], synth_horizontal[1]), 5, (255,255,255), 4)
 		cv2.circle(orig_img, (synth_horizontal[2], synth_horizontal[3]), 5, (255,255,255), 4)
 
-	if first_pole != [] and second_pole != []:
+	if first_pole != [] and second_pole != []: 
+		"""
+		# I know this could be condensed a lot more but it is easier to look at this way
+		if first1_top == True: # all of the if statements are to make sure the cross goes diagnally 
+			if second1_top == True:
+				cross1 = [ext1[0], ext1[1], second_pole[2], second_pole[3]]
+				cross2 = [ext2[0], ext2[1], first_pole[2], first_pole[3]]
+			elif second1_top == False:
+				cross1 = [ext1[0], ext1[1], second_pole[0], second_pole[1]]
+				cross2 = [ext2[0], ext2[1], first_pole[2], first_pole[3]]
+		elif first1_top == False:
+			if second1_top == True:
+				cross1 = [ext1[0], ext1[1], second_pole[2], second_pole[3]]
+				cross2 = [ext2[0], ext2[1], first_pole[0], first_pole[1]]
+			elif second1_top == False:
+				cross1 = [ext1[0], ext1[1], second_pole[0], second_pole[1]]
+				cross2 = [ext2[0], ext2[1], first_pole[0], first_pole[1]]
+		
+		center = line_intersection(cross1, cross2, debug)
+
+		if debug == True:
+			cv2.line(orig_img, (cross1[0],cross1[1]),(cross1[2], cross1[3]),(0,237,0),1)
+			cv2.line(orig_img, (cross2[0],cross2[1]),(cross2[2], cross2[3]),(0,237,0),1)
+			cv2.circle(orig_img, (center[0], center[1]), 5, (255,255,255), 4)
+		"""
 		# calculate center of first_pole
 		first_pole_mid = []
 		first_pole_mid.append(float(first_pole[0] + first_pole[2])/2)
@@ -362,30 +386,38 @@ def touch_up(first_pole, second_pole, horizontal, orig_img, confidence, debug):
 		x_mid_line.append(second_pole_mid[1])
 
 		if debug == True:
-			cv2.line(orig_img, (int(x_mid_line[0]),int(x_mid_line[1])),(int(x_mid_line[2]),int(x_mid_line[3])),(177,237,157),2) # some lime green color
+			cv2.line(orig_img, (int(x_mid_line[0]),int(x_mid_line[1])),(int(x_mid_line[2]),int(x_mid_line[3])),(0,237,0),2) # some lime green color
 
+		
 		# find verticle center of gate
 		hor_mid = []
 
 		# find center between poles
-		if horizontal != []:
+		if len(horizontal) == 4:
 			hor_mid.append(float(ext1[0] + ext2[0])/2)
 			hor_mid.append(float(ext1[1] + ext2[1])/2)
-		elif synth_horizontal != []:
+		elif len(synth_horizontal) == 4:
 			hor_mid.append(float(synth_horizontal[0] + synth_horizontal[2])/2)
 			hor_mid.append(float(synth_horizontal[1] + synth_horizontal[3])/2)
-
+		
 		# create line
-		if horizontal != []:
+		if len(horizontal) == 4:
 			hor_slope = find_slope_singleline(horizontal)
-		elif synth_horizontal != []:
+		elif len(synth_horizontal) == 4:
 			hor_slope = find_slope_singleline(synth_horizontal)
 		
+	
+		new_slope_rad = slope_to_rad(hor_slope) + (np.pi/2) # turn 90deg
+		new_slope = rad_to_slope(new_slope_rad) # convert back to rise over run
+
+		#cv2.line(orig_img, (0,0),(int(hor_mid[2]),int(hor_mid[3])),(177,237,157),2)
+
 		temp_coord = []
-		temp_coord_b = hor_mid[1]-hor_slope*hor_mid[0]
-		temp_coord_x = hor_mid[0] + 1
-		# I need to fix this line so it is perpendicular to horizontal bar. Right now, it goes straight now which is good enough
-		temp_coord_y = temp_coord_x * (-1/hor_slope) + temp_coord_b # math.sqrt(3**2/(hor_slope**2+1))
+		temp_coord_b = hor_mid[1]-new_slope*hor_mid[0]
+
+		temp_coord_y = first_pole[3] + 100 # make sure it calculate under the other horzontal center line
+		temp_coord_x = (temp_coord_y - temp_coord_b)/new_slope
+		
 		temp_coord.append(temp_coord_x)
 		temp_coord.append(temp_coord_y)
 
@@ -393,21 +425,87 @@ def touch_up(first_pole, second_pole, horizontal, orig_img, confidence, debug):
 		hor_mid.append(temp_coord_y)
 
 		if debug == True:
-			cv2.line(orig_img, (int(hor_mid[0]),int(hor_mid[1])),(int(hor_mid[2]),int(hor_mid[3])),(177,237,157),2) # some lime green color
+			cv2.line(orig_img, (int(hor_mid[0]),int(hor_mid[1])),(int(hor_mid[2]),int(hor_mid[3])),(0,237,0),2) # some lime green color
 
 
 		# calculate center of gate (from new lines)
-		center = line_intersection(x_mid_line, hor_mid)
+		center = line_intersection(x_mid_line, hor_mid, debug)
 
 		cv2.circle(orig_img, (int(center[0]), int(center[1])), 3, (255,255,255), 4)
 
-	print(orig_img)
-	print(ext_percent)
-	print(first_pole)
-	print(second_pole)
-	print(center)
-	return(orig_img, ext_percent, first_pole, second_pole, center)
+		# determain sides of gate
+		left_side, right_side = split_gate(hor_mid, first_pole, second_pole, first1_top, second1_top, horizontal, synth_horizontal, debug)
 
+		# add sides to img
+		if debug == True:
+			cv2.rectangle(orig_img, (int(left_side[0]),int(left_side[1])), (int(left_side[2]),int(left_side[3])), (0,0,0), 1)
+			cv2.rectangle(orig_img, (int(right_side[0]),int(right_side[1])), (int(right_side[2]),int(right_side[3])), (255,255,255), 1)
+			
+	return(orig_img, ext_percent, first_pole, second_pole, center, synth_horizontal)
+
+def add_legend(debug, orig_img, first_pole, second_pole, horizontal, synth_horizontal):
+	# add legend
+	spacing = 25 # add 20 every time you put text
+	if debug == True:
+		cv2.putText(orig_img, 'all narrowed down lines', (5,spacing), cv2.FONT_HERSHEY_SIMPLEX,  0.75, (0,0,157), 1, cv2.LINE_AA)
+		spacing = spacing + 20
+		if first_pole != [] or second_pole != []:
+			cv2.putText(orig_img, 'poles', (5,spacing), cv2.FONT_HERSHEY_SIMPLEX,  0.75, (0,0,255), 1, cv2.LINE_AA)
+			spacing = spacing + 20 
+			if len(horizontal) == 4:
+				cv2.putText(orig_img, 'horizontal har (detected)', (5,spacing), cv2.FONT_HERSHEY_SIMPLEX,  0.75, (255,0,0), 1, cv2.LINE_AA)
+				spacing = spacing + 20
+				cv2.putText(orig_img, 'horizontal bar (filled)', (5,spacing), cv2.FONT_HERSHEY_SIMPLEX,  0.75, (177,237,157), 1, cv2.LINE_AA)
+				spacing = spacing + 20
+			if len(synth_horizontal) == 4:
+				cv2.putText(orig_img, 'predicted horizontal bar', (5,spacing), cv2.FONT_HERSHEY_SIMPLEX,  0.75, (177,237,157), 1, cv2.LINE_AA)
+				spacing = spacing + 20
+			if first_pole != [] and second_pole != []:
+				cv2.putText(orig_img, 'gate midlines', (5,spacing), cv2.FONT_HERSHEY_SIMPLEX,  0.75, (0,237,0), 1, cv2.LINE_AA)
+				spacing = spacing + 20
+				cv2.putText(orig_img, 'left side', (5,spacing), cv2.FONT_HERSHEY_SIMPLEX,  0.75, (0,0,0), 1, cv2.LINE_AA)
+				spacing = spacing + 20
+				cv2.putText(orig_img, 'right side', (5,spacing), cv2.FONT_HERSHEY_SIMPLEX,  0.75, (255,255,255), 1, cv2.LINE_AA)
+
+def split_gate(hor_mid, first_pole, second_pole, first1_top, second1_top, horizontal, synth_horizontal, debug):
+	if horizontal != []:
+		hor = True
+	elif synth_horizontal != []:
+		hor = False
+	
+	# x1/y1/x2/y2 coordnates (for two corners)
+	left_side = [] 
+	right_side = []
+
+	if len(hor_mid) == 4:
+		# add bottom left corner of first pole to list
+		if first1_top == True:
+			left_side.append(first_pole[2])
+			left_side.append(first_pole[3])
+		elif first1_top == False:
+			left_side.append(first_pole[0])
+			left_side.append(first_pole[1])
+		
+		# add middle of horizontal bar to list
+		left_side.append(hor_mid[0])
+		left_side.append(hor_mid[1])
+
+		# add bottom right corner of second pole to list
+		if second1_top == True:
+			right_side.append(second_pole[2])
+			right_side.append(second_pole[3])
+		elif second1_top == False:
+			right_side.append(second_pole[0])
+			right_side.append(second_pole[1])
+		
+		# add middle of horizontal bar to list
+		right_side.append(hor_mid[0])
+		right_side.append(hor_mid[1])
+	else:
+		if debug == True:
+			print("Gate sides cannot be determained.")
+
+	return(left_side, right_side)
 
 def confidence(x1_list, y1_list, x2_list, y2_list, refined_slopes, first_pole, second_pole, ext_percent):
 
@@ -464,6 +562,8 @@ def confidence(x1_list, y1_list, x2_list, y2_list, refined_slopes, first_pole, s
 
 	return(final_confidence)
 
+# record start time
+start_time = time.perf_counter()
 
 # make sure terminal line that calls this script has the right number of contents
 if len(sys.argv) != 3:
@@ -526,8 +626,10 @@ if horizontal != []:
 
 cv2.imshow("rough detection", orig_img)
 """
-final, ext_percent, first_pole, second_pole, center = touch_up(first_pole, second_pole, horizontal, orig_img, confidence, debug)
+final, ext_percent, first_pole, second_pole, center, synth_horizontal = touch_up(first_pole, second_pole, horizontal, orig_img, confidence, debug)
 
+# add legend to image
+add_legend(debug, orig_img, first_pole, second_pole, horizontal, synth_horizontal)
 
 # add lines to image # FIS THIS NEW IMG VARIABLE
 if first_pole != []:
@@ -548,6 +650,11 @@ if debug == True:
 	print("second_pole: " + str(second_pole))
 
 cv2.imshow("final detection - " + str(confidence), final)
+
+# calc total time
+end_time = time.perf_counter()
+tot_time = round(end_time - start_time, 3)
+print("Took " + str(tot_time) + " sec")
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
